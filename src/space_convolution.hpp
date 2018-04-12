055437f0f145ec103ce92f9dbb3df0e514fd5abc
Isaac Núñez <shak04995@gmail.com>
2018-04-12 07:51:48 -0600

    Delete unneccessary files

--- src/space_convolution.hpp
+++ src/space_convolution.hpp
@@ -25,7 +25,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "time_measure.hpp"
 
 /**
  * Spacial Filtering using non-separable kernel with a size nx1. It also recieves an
